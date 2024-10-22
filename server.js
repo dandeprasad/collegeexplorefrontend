9c847e9a15c3b83c47feac56a874aacafcd19cdd
Dande Reddy Prasad <dandereddyprasad@gmail.com>
2024-10-22 12:49:25 +0530

    dandekingmas
    dandeking

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+
 app.get('*',(req,res) => {
   res.sendFile(path.resolve(__dirname,'index.html'))
 });
+
 
 app.listen(port);
 console.log('Server started');
